cd00f94595a1ec6620be8ea52f2f7a7d38283635
Md. Sabbir Hossain <82496216+SABBIR-H0SSAIN@users.noreply.github.com>
2025-07-12 23:29:38 +0600

    Fixed error while connecting to neon db

--- db/index.ts
+++ db/index.ts
@@ -1,14 +1,5 @@
 import { config } from "@/config";
 import { drizzle } from "drizzle-orm/node-postgres";
-import { Pool } from "pg";
 import * as schema from "./schemas";
 
-export const pool = new Pool({
-  connectionString: config.db.url,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-  max: 10,
-});
-
-export const db = drizzle(pool, { schema });
+export const db = drizzle(config.db.url, { schema });
